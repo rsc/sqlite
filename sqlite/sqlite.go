d306ad21a6a8711eea1e655b1236422877be92eb
Russ Cox <rsc@swtch.com>
2011-01-23 17:42:43 -0500

    backup

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -41,7 +41,7 @@ type Errno int
 func (e Errno) String() string {
 	s := errText[e]
 	if s == "" {
-		return fmt.Sprintf("errno %d", e)
+		return fmt.Sprintf("errno %d", int(e))
 	}
 	return s
 }
@@ -110,6 +110,9 @@ func (c *Conn) error(rv C.int) os.Error {
 	if c == nil || c.db == nil {
 		return os.NewError("nil sqlite database")
 	}
+	if rv == 0 {
+		return nil
+	}
 	if rv == 21 {	// misuse
 		return Errno(rv)
 	}
@@ -145,6 +148,65 @@ func Open(filename string) (*Conn, os.Error) {
 		return nil, os.NewError("sqlite succeeded without returning a database")
 	}
 	return &Conn{db}, nil
+}
+
+func NewBackup(dst *Conn, dstTable string, src *Conn, srcTable string) (*Backup, os.Error) {
+	dname := C.CString(dstTable)
+	sname := C.CString(srcTable)
+	defer C.free(unsafe.Pointer(dname))
+	defer C.free(unsafe.Pointer(sname))
+
+	sb := C.sqlite3_backup_init(dst.db, dname, src.db, sname)
+	if sb == nil {
+		return nil, dst.error(C.sqlite3_errcode(dst.db))
+	}
+	return &Backup{sb, dst, src}, nil
+}
+
+type Backup struct {
+	sb *C.sqlite3_backup
+	dst, src *Conn
+}
+
+func (b *Backup) Step(npage int) os.Error {
+	rv := C.sqlite3_backup_step(b.sb, C.int(npage))
+	if rv == 0 || Errno(rv) == ErrBusy || Errno(rv) == ErrLocked {
+		return nil
+	}
+	return Errno(rv)
+}
+	
+type BackupStatus struct {
+	Remaining int
+	PageCount int
+}
+
+func (b *Backup) Status() BackupStatus {
+	return BackupStatus{int(C.sqlite3_backup_remaining(b.sb)), int(C.sqlite3_backup_pagecount(b.sb))}
+}
+
+func (b *Backup) Run(npage int, sleepNs int64, c chan<- BackupStatus) os.Error {
+	var err os.Error
+	for {
+		err = b.Step(npage)
+		if err != nil {
+			break
+		}
+		if c != nil {
+			c <- b.Status()
+		}
+		time.Sleep(sleepNs)
+	}
+	return b.dst.error(C.sqlite3_errcode(b.dst.db))		
+}
+
+func (b *Backup) Close() os.Error {
+	if b.sb == nil {
+		return os.EINVAL
+	}
+	C.sqlite3_backup_finish(b.sb)
+	b.sb = nil
+	return nil
 }
 
 func (c *Conn) Exec(cmd string, args ...interface{}) os.Error {
